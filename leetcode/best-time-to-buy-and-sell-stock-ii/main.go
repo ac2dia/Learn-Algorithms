5822c56f69ca6be2f79cbead0c728abced8ab622
ac2dia <ac2dia@gmail.com>
2023-01-09 05:59:28 +0900

    [leetcode] Best Time to Buy and Sell Stock II
    - go test -coverprofile=coverage.out
    - go tool cover -html=coverage.out -o cover.html

--- leetcode/best-time-to-buy-and-sell-stock-ii/main.go
+++ leetcode/best-time-to-buy-and-sell-stock-ii/main.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-	prices := []int{7, 1, 5, 3, 6, 4}
-	totalProfit := maxProfit(prices)
-
-	fmt.Println(totalProfit)
-}
-
 func maxProfit(prices []int) int {
 
 	totalProfit := 0
